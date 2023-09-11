52b0323d4f0a5ca524046813273bc02edf3506aa
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-09-11 08:49:54 +0300

    Add filtering and ordering

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.ReqestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -9,19 +10,47 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
-        int pageNumber = 1, int pageSize = 4)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.PagedSearch<Item>();
+        var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
-
-        if (!string.IsNullOrEmpty(searchTerm))
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
-        query.PageNumber(pageNumber).PageSize(pageSize);
+        // order by
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)), // sort by make
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)), // newest first
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)) // default sort by auction end date
+        };
+
+        // filter by
+
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow), // finished auctions
+            "endingSoon" => query.Match(x => x.AuctionEnd > DateTime.UtcNow
+                && x.AuctionEnd < DateTime.UtcNow.AddHours(6)), // auctions ending soon
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow) // default: active auctions
+        };
+
+        // seller
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        // winner
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+
+        query.PageNumber(searchParams.PageNumber).PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 

bde8f8d4a7ded66a3664235ce01adeb501d4e1b0
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-09-12 11:27:03 +0300

    Add Http communication to get the data

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -20,17 +21,30 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("No Data - will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        // if (count == 0)
+        // {
+        //     Console.WriteLine("No Data - will attempt to seed");
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
 
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
 
-            await DB.SaveAsync(items);
-        }
+        //     await DB.SaveAsync(items);
+        // }
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " returned from AuctionService");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+
+
+
 
     }
 }

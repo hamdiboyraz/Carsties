f71a0fb9688162143f5d490c2164a11fbbf8951a
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-09-23 19:47:03 +0300

    Fix proxy-https bug

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -6,13 +6,13 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.Authority = builder.Configuration["IdentityServiceUrl"];
-    options.RequireHttpsMetadata = false; // We are using http in dev env
-    options.TokenValidationParameters.ValidateAudience = false; // We are not validating audience in dev env
-    options.TokenValidationParameters.NameClaimType = "username";
-});
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false; // We are using http in dev env
+        options.TokenValidationParameters.ValidateAudience = false; // We are not validating audience in dev env
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
 
 var app = builder.Build();
 

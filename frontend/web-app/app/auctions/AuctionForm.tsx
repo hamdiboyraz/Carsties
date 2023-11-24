8f10f073e3e27f3801aeb4956f145faa3ec5a135
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-11-24 23:51:19 +0300

    Create reusable Input component and refactor

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -2,10 +2,11 @@
 import { Button, TextInput } from "flowbite-react";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 export default function AuctionForm() {
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
@@ -17,7 +18,19 @@ export default function AuctionForm() {
 
   return (
     <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3 block">
+      <Input
+        label="Make"
+        name="make"
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      {/* <div className="mb-3 block">
         <TextInput
           {...register("make", { required: "Make is required" })}
           placeholder="Make"
@@ -32,7 +45,7 @@ export default function AuctionForm() {
           color={errors.model && "failure"}
           helperText={errors.model?.message as string}
         />
-      </div>
+      </div> */}
       <div className="flex justify-between">
         <Button outline color="gray">
           Cancel

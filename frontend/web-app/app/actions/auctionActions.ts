16f73a754e7ffd7d46aa4bc67ffe754569c0b41b
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-11-30 02:58:25 +0300

    Add edit functionality

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,6 +2,7 @@
 import { Auction, PagedResult } from "@/types";
 import { fetchWrapper } from "@/lib/fetchWrapper";
 import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   return await fetchWrapper.get(`search${query}`);
@@ -24,4 +25,10 @@ export async function createAuction(auction: FieldValues) {
 
 export async function getDetailedViewData(id: string): Promise<Auction> {
   return await fetchWrapper.get(`auctions/${id}`);
+}
+
+export async function updateAuction(id: string, auction: FieldValues) {
+  const res = await fetchWrapper.put(`auctions/${id}`, auction);
+  revalidatePath(`/auctions/${id}`);
+  return res;
 }

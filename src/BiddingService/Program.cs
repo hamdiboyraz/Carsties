e0ec3a5ad48a7f0e283538ac87a77812658ecdb0
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-12-01 00:29:13 +0300

    Simplify and configure BiddingService

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,25 +1,46 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h => // h stands for host
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest")); // default username is guest
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest")); // default password is guest
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false; // We are using http in dev env
+        options.TokenValidationParameters.ValidateAudience = false; // We are not validating audience in dev env
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("BidDb", MongoClientSettings.
+    FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
 
 app.Run();
